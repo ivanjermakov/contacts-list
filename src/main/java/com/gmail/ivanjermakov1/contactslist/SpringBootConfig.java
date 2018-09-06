2085227bad65475b4e3e72405dc11c52a9fa4ea9
vaniashows <vania6600@gmail.com>
2018-09-06 14:17:02 +0300

    init

---
+++ src/main/java/com/gmail/ivanjermakov1/contactslist/SpringBootConfig.java
@@ -0,0 +1,13 @@
+package com.gmail.ivanjermakov1.contactslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfig {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfig.class, args);
+	}
+	
+}
