6447f359f847b87349f8ee0ee8e4be11130bb5c7
vaniashows <vania6600@gmail.com>
2018-09-08 13:23:45 +0300

    contacts list web page design

--- src/main/java/com/gmail/ivanjermakov1/contactslist/SpringBootConfig.java
+++ src/main/java/com/gmail/ivanjermakov1/contactslist/SpringBootConfig.java
@@ -2,7 +2,6 @@ package com.gmail.ivanjermakov1.contactslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringBootConfig {
